485cb7b6045ffca5d3961b8d4d307b4834729b95
Azraf Chowdhury <achowdhur@ltu.edu>
2019-03-13 04:22:51 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,7 +3,7 @@ class PigLatinizer
  def piglatinize(word)
    non_pig_latin_words = ["i", "me", "to", "too", "a", "an", "in", "and", "on",]
    vowels = ["a", "e", "i", "o", "u"]
- end 
- 
+ end
 
-end 
+
+end
